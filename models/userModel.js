fd35ddf88173661de87b75d0ff1c710c1897670c
thefx3 <fx.pairault@yahoo.fr>
2025-11-16 16:13:06 +0100

    Created UserModel

---
+++ models/userModel.js
@@ -0,0 +1,60 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+class UserModel {
+    async createUser(data) {
+        return await prisma.user.create({
+            data: {
+              username: data.username,
+              email: data.email,
+              password: data.password, // hash bcrypt
+              role: data.role ?? "USER",
+            },
+          });
+    }
+
+    async updateUser(id, data) {
+        return await prisma.user.update({
+            where: { id },
+            data: {
+              ...(data.username && { username: data.username }),
+              ...(data.email && { email: data.email }),
+              ...(data.password && { password: data.password }), // hash bcrypt
+            },
+          });
+    }
+
+    async updateUserRole(id, newrole) {
+        return await prisma.user.update({
+            where: { id: id },
+            data: { role: newrole },
+          })
+    }
+
+    async deleteUser(id) {
+      return await prisma.user.delete({
+        where: { id: id },
+      })
+    }
+
+    async getUserByEmail(email) {
+      return await prisma.user.findUnique({
+        where: { email: email },
+      })
+    }
+
+    async getUserByUsername(username) {
+        return await prisma.user.findUnique({
+            where: { username: username },
+          })
+    }
+
+    async getUserById(id) {
+        return await prisma.user.findUnique({
+            where: { id: id },
+          })
+    }
+}
+
+
+module.exports = new UserModel(); 
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ const router = Router();
 const upload = multer({ dest: 'uploads/' })
 const multer  = require('multer')
 
+const authIndex = require('../routes/authIndex');
+
 // ----------- GET ROUTES --------------
-
-
+router.get('/', authIndex);
 // ----------- POST ROUTES -------------
 
 
